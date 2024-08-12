95a1fa7925befe612c9ef3098792d971b30029c9
DaniellyCN <daniellyccn@gmail.com>
2024-08-12 12:17:37 -0300

    intercaoa com serviço back

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <PdfChat/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PdfChat from './components/PdfChat.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PdfChat
   }
 }
 </script>
--- src/components/PdfChat.vue
+++ src/components/PdfChat.vue
@@ -3,8 +3,95 @@
 </script>
 
 <template>
-  $END$
+  <div>
+    <h1>Conversar com o PDF</h1>
+
+    <form @submit.prevent="uploadPdf">
+      <input type="file" @change="handleFileChange" />
+      <button type="submit">Carregar PDF</button>
+    </form>
+
+    <div v-if="pdfText">
+      <h2>Texto do PDF</h2>
+      <textarea v-model="pdfText" rows="10" cols="50" readonly></textarea>
+
+      <h2>Faça uma pergunta</h2>
+      <form @submit.prevent="askQuestion">
+        <input type="text" v-model="question" placeholder="Digite sua pergunta" />
+        <button type="submit">Perguntar</button>
+      </form>
+    </div>
+
+    <div v-if="response">
+      <h2>Resposta</h2>
+      <p>{{ response }}</p>
+    </div>
+
+    <div v-if="message">
+      <p>{{ message }}</p>
+    </div>
+  </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      name: 'PdfChat',
+      props: {
+        msg: String
+      },
+      file: null,
+      pdfText: '',
+      question: '',
+      response: '',
+      message: ''
+    };
+  },
+  methods: {
+    handleFileChange(event) {
+      this.file = event.target.files[0];
+    },
+    async uploadPdf() {
+      const formData = new FormData();
+      formData.append('file', this.file);
+
+      try {
+        const response = await axios.post('http://localhost:8080/api/pdf', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+        });
+        this.pdfText = true;
+        this.message = response.data.message;
+      } catch (error) {
+        this.message = 'Erro ao carregar o PDF: ' + error.message;
+      }
+    },
+    async askQuestion() {
+      const formData = new FormData();
+      formData.append('pergunta', this.question);
+      try {
+        const response = await axios.get('http://localhost:8080/api/chat/1', {
+          params: {
+            pergunta: this.question
+          }
+        });
+        this.response = response.data;
+      } catch (error) {
+        this.message = 'Erro ao enviar a pergunta: ' + error.message;
+      }
+    }
+  }
+};
+
+
+
+</script>
+
+
 
 <style scoped>
 
